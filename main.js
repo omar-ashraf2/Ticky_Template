607614a70b7631267d27d57e767c88fdf0c119dc
Omar Ashraf <oashraf304@gmail.com>
2023-01-13 13:55:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ document.onkeyup = function (e) {
   }
 };
 
-let countDown = new Date("Dec 31, 2022 23:59:59").getTime();
+let countDown = new Date("Dec 31, 2023 23:59:59").getTime();
 
 let counter = setInterval(() => {
   let dateNow = new Date().getTime();
